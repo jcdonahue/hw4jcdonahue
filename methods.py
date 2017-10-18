9578de21f52591ffa91d24359a1ef0419eb90405
jcdonahue <jackdnhue@gmail.com>
2017-10-17 21:20:20 -0400

    seperated out definitions, added halley's commet

--- methods.py
+++ methods.py
@@ -4,30 +4,76 @@ import numpy as np
 import math
 import leastsquares as ls
 
-def feul(h,y0):
-    y = y0 + h*df(y0)
-    return y
+def feul(h,y0,N):
+    track = np.zeros([N,4])
+    track[0,:] = y0
+    for j in range(N-1):
+        y0 = y0 + h*df(y0)
+        track[j+1,:] = y0
+    return track
 
-def rk2(h,y0):
-    k1 = h*df(y0)
-    k2 = h*df(y0+0.5*k1)
-    y0 = y0 + k2
-    return y0
+def rk2(h,y0,N):
+    track = np.zeros([N,4])
+    track[0,:] = y0
+    for j in range(N-1):
+        k1 = h*df(y0)
+        k2 = h*df(y0+0.5*k1)
+        y0 = y0 + k2
+        track[j+1,:] = y0
+    return track
 
-def rk4(h,y0):
-    k1 = h*df(y0)
-    k2 = h*df(y0+0.5*k1)
-    k3 = h*df(y0+0.5*k2)
-    k4 = h*df(y0+k3)
-    y0 = y0 + k1/6.0 + k2/3.0 + k3/3.0 + k4/6.0
-    return y0
+def rk4(h,y0,N):
+    track = np.zeros([N,4])
+    track[0,:] = y0
+    for j in range(N-1):
+        k1 = h*df(y0)
+        k2 = h*df(y0+0.5*k1)
+        k3 = h*df(y0+0.5*k2)
+        k4 = h*df(y0+k3)
+        y0 = y0 + k1/6.0 + k2/3.0 + k3/3.0 + k4/6.0
+        track[j+1,:] = y0
+    return track
 
-def verlet(h,y0,v):
-    y0[0:2] = y0[:2] + h*v
-    k = h*df(y0)[2:]
-    y0[2:] = v+0.5*k
-    v = v+k
-    return np.array([y0,v])
+def adaprk4(h,r,T):
+    beta = 0.9
+    ep0 = np.array([1e-10,1e-10,1e-10,1e-10])
+    ep0min = min(ep0)
+    t = 0
+    track = [r]
+    count = 0
+    while t < T:
+        if count > 100:
+            print("I got stuck")
+            break
+        else:
+            r1 = rk4(h,r,2)[-1]
+            r2 = rk4(0.5*h,r,3)[-1]
+            ep = np.abs(r1-r2)/15
+            epmax = max(ep)
+            if np.all(ep0>ep):
+                t += h
+                track  = np.append(track,[r1],axis=0)
+                h = beta*h*np.power((ep0min/epmax),0.2)
+                r = r1
+                count = 0
+            else:
+                h = beta*h*np.power((ep0min/epmax),0.25)
+                count += 1
+    return track
+
+def verlet(h,r,N):
+    track = np.zeros([N,4])
+    track[0,:] = r
+    vhalf = r[2:]+0.5*h*df(r)[2:]
+    y0 = np.zeros(4)
+    for j in range(N-1):
+        y0[0:2] = r[:2] + h*vhalf
+        k = h*df(r)[2:]
+        y0[2:] = vhalf+0.5*k
+        vhalf = vhalf+k
+        r = y0
+        track[j+1,:] = r
+    return track
 
 def df(r):
     rad = np.sqrt(np.power(r[0],2)+np.power(r[1],2))
@@ -38,20 +84,18 @@ def df(r):
     df[3] = -4*np.power(np.pi, 2)*r[1]*np.power(rad,-3)
     return df
 
-def tomin(x,T):
-    a = 1.523679
-    e = 0.0934
+def tomin(x,T,a,e):
     semmaj = a/(1+e)
     x = x-e*np.sin(x)-T*np.sqrt(np.power(semmaj,-3))
     return x
 
-def newraph(N,T):
-    a = 1.523679
-    e = 0.0934
+def newraph(N,T,a,e):
+    # a = 1.523679
+    # e = 0.0934
     semmaj = a/(1+e)
     x = 0
     for i in range(N):
-        x = x - (tomin(x,T))/(1-e*np.cos(x))
+        x = x - (tomin(x,T,a,e))/(1-e*np.cos(x))
     r = semmaj*(1-e*np.cos(x))
     return r
 
@@ -63,7 +107,7 @@ def secant(N,T):
     x1 = 0
     x2 = np.pi*0.5
     for i in range(N):
-        x = x2 - tomin(x2,T)*(x2-x1)/(tomin(x2,T)-tomin(x1,T))
+        x = x2 - tomin(x2,T,a,e)*(x2-x1)/(tomin(x2,T,a,e)-tomin(x1,T,a,e))
         x1 = x2
         x2 = x
         if np.abs(x2-x1)<tol:
@@ -77,7 +121,7 @@ def relax(N,T):
     semmaj = a/(1+e)
     x = 0
     for i in range(N):
-        x = x - tomin(x,T)
+        x = x - tomin(x,T,a,e)
     r = semmaj*(1-e*np.cos(x))
     return r
 
@@ -88,13 +132,13 @@ def bisect(N,T):
     semmaj = a/(1+e)
     x1 = 0
     x2 = 2*np.pi
-    fx1 = tomin(x1,T)
-    fx2 = tomin(x2,T)
+    fx1 = tomin(x1,T,a,e)
+    fx2 = tomin(x2,T,a,e)
     count = 0
     while count<N:
         if np.sign(fx1).astype(int) == - np.sign(fx2).astype(int):
             xnew = 0.5*(x1+x2)
-            fxnew = tomin(xnew,T)
+            fxnew = tomin(xnew,T,a,e)
             if  np.sign(fx1).astype(int) == np.sign(fxnew).astype(int):
                 x1 = xnew
                 fx1 = fxnew
@@ -113,106 +157,3 @@ def bisect(N,T):
     x = 0.5*(x1+x2)
     r = semmaj*(1-e*np.cos(x))
     return r
-
-def orbital(N,T,i):
-    h = T/N
-    a = 1.523679
-    e = 0.0934
-    semmaj = a/(1+e)
-    semmin = semmaj*np.sqrt(1-np.power(e,2))
-    r = np.zeros(4)
-    f = np.zeros(4)
-    track = np.zeros([N,4])
-    r[0] = -a*(1+e)
-    r[3] = 2*np.pi*np.sqrt((1-e)/(a*(1+e)))
-    if i == 0:
-        for j in range(N):
-            track[j,:] = r
-            r = feul(h,r)
-    if i == 1:
-        for j in range(N):
-            track[j,:] = r
-            r = rk2(h,r)
-    if i == 2:
-        for j in range(N):
-            track[j,:] = r
-            r = rk4(h,r)
-    if i == 3:
-        track[0,:] = r
-        vhalf = r[2:]+0.5*h*df(r)[2:]
-        for j in range(N-1):
-            inter = verlet(h,r,vhalf)
-            r = inter[0]
-            vhalf = inter[1]
-            track[j+1,:] = r
-    if i == 4:
-        beta = 0.9
-        ep0 = np.array([1e-12,1e-12,1e-12,1e-12])
-        ep0min = min(ep0)
-        t = 0
-        track = [r]
-        count = 0
-        while t < T:
-            if count > 100:
-                print("I got stuck")
-                break
-            else:
-                r1 = rk4(h,r)
-                r2 = rk4(0.5*h,r)
-                r3 = rk4(0.5*h,r2)
-                ep = np.abs(r1-r3)/15
-                epmax = max(ep)
-                if np.all(ep0>ep):
-                    t += h
-                    track  = np.append(track,[r1],axis=0)
-                    h = beta*h*np.power((ep0min/epmax),0.2)
-                    r = r1
-                    count = 0
-                else:
-                    h = beta*h*np.power((ep0min/epmax),0.25)
-                    count += 1
-    rad = np.sqrt(np.power(track[:,0],2)+np.power(track[:,1],2))
-    E = .5*(np.power(track[:,3],2)+np.power(track[:,2],2))-np.power(rad,-1)
-    fig1, ax1 = plt.subplots(2,1)
-    ax1[0].plot(E,'k-')
-    ax1[1].plot(track[:,0],track[:,1])
-    return rad[-1]
-
-def errorplot(N,T,filename=None):
-    terms = len(N)
-    a = np.zeros([terms,5])
-    exact = newraph(1000,5)
-    for i in range(terms):
-        for j in range(5):
-            a[i,j] = np.abs(orbital(N[i],T,j)-exact)
-    lN = np.log10(N)
-    la = np.log10(a)
-    least  = np.zeros([5,2])
-    for j in range(5):
-        least[j,:] = ls.leastsquares(lN,la[:,j])
-    
-    fig1, ax1 = plt.subplots(1,1)
-    # ax1.plot(lN, la[:,0],'ko')
-    # p0, = ax1.plot(lN,least[0,0]*lN+least[0,1],'k-',label = '%.4f' % least[0,0])
-    # ax1.set_title("t = "+str(T))
-  
-    # ax1.plot(lN, la[:,1],'ro')
-    # p1, = ax1.plot(lN,least[1,0]*lN+least[1,1],'r-',label = '%.4f RK2' % least[1,0])
-
-    # ax1.plot(lN, la[:,2],'bo')
-    # p2, = ax1.plot(lN,least[2,0]*lN+least[2,1],'b-',label = '%.4f RK4' % least[2,0])
-
-    # ax1.plot(lN, la[:,3],'go')
-    # p3, = ax1.plot(lN,least[3,0]*lN+least[3,1],'g-',label = '%.4f Verlet' % least[3,0])
-
-    # ax1.plot(lN, la[:,4],'mo')
-    # p4, = ax1.plot(lN,least[4,0]*lN+least[4,1],'m-',label = '%.4f Adaptive RK4' % least[4,0])
-    # l0 = ax1.legend(handles=[p1,p2,p3,p4],loc=1)  
-
-
-    # ax1.set_ylabel(r'$r$ Log Error')
-    # ax1[1].set_ylabel(r'RK2')
-    # ax1[2].set_ylabel(r'RK4')
-    # ax1[3].set_ylabel(r'Verlet')
-    # ax1[4].set_ylabel(r'Adaptive RK4')
-    # ax1[4].set_xlabel(r'Log $N$')
---
+++ orbits.py
@@ -0,0 +1,97 @@
+from __future__ import division
+import pylab as plt
+import numpy as np
+import math
+import time
+import leastsquares as ls
+from methods import feul,rk2,rk4,verlet,df,tomin,secant,newraph,relax,bisect,adaprk4
+
+
+def orbital(N,T,i):
+    h = T/N
+    a = 1.523679
+    e = 0.0934
+    r = np.zeros(4)
+    r[0] = -a*(1+e)
+    r[3] = 2*np.pi*np.sqrt((1-e)/(a*(1+e)))
+    if i == 0:
+        track = feul(h,r,N)
+    if i == 1:
+        track = rk2(h,r,N)
+    if i == 2:
+        track = rk4(h,r,N)
+    if i == 3:
+        track = verlet(h,r,N)
+    if i == 4:
+        track = adaprk4(h,r,T)
+    rad = np.sqrt(np.power(track[:,0],2)+np.power(track[:,1],2))
+    E = .5*(np.power(track[:,3],2)+np.power(track[:,2],2))-np.power(rad,-1)
+    fig1, ax1 = plt.subplots(2,1)
+    ax1[0].plot(E,'k-')
+    ax1[1].plot(track[:,0],track[:,1])
+    return rad[-1]
+
+def halley(N,pers):
+    period = 75
+    T = pers*period
+    h = T/N
+    a = 35.0823104
+    e = .96714
+    r = np.zeros(4)
+    r[0] = -a*(1+e)
+    r[3] = 2*np.pi*np.sqrt((1-e)/(a*(1+e)))
+    t1 = time.time()
+    track1 = rk4(h,r,N)
+    t2 = time.time()
+    track2 = verlet(h,r,N)
+    t3 = time.time()
+    track3 = adaprk4(h,r,T)
+    t4 = time.time()
+    #rad = np.sqrt(np.power(track[:,0],2)+np.power(track[:,1],2))
+    #E = .5*(np.power(track[:,3],2)+np.power(track[:,2],2))-np.power(rad,-1)
+    fig1, ax1 = plt.subplots(1,1)
+    #ax1[0].plot(E,'k-')
+    ax1.plot(track1[:,0],track1[:,1],'b-')
+    ax1.plot(track2[:,0],track2[:,1],'g-')
+    ax1.plot(track3[:,0],track3[:,1],'k-')
+    return (t2-t1,t3-t2,t4-t3)
+
+
+def errorplot(N,T,filename=None):
+    terms = len(N)
+    a = np.zeros([terms,5])
+    exact = newraph(1000,5,1.523679,0.0934)
+    for i in range(terms):
+        for j in range(5):
+            a[i,j] = np.abs(orbital(N[i],T,j)-exact)
+    lN = np.log10(N)
+    la = np.log10(a)
+    least  = np.zeros([5,2])
+    for j in range(5):
+        least[j,:] = ls.leastsquares(lN,la[:,j])
+    
+    fig1, ax1 = plt.subplots(1,1)
+    # ax1.plot(lN, la[:,0],'ko')
+    # p0, = ax1.plot(lN,least[0,0]*lN+least[0,1],'k-',label = '%.4f' % least[0,0])
+    # ax1.set_title("t = "+str(T))
+  
+    # ax1.plot(lN, la[:,1],'ro')
+    # p1, = ax1.plot(lN,least[1,0]*lN+least[1,1],'r-',label = '%.4f RK2' % least[1,0])
+
+    # ax1.plot(lN, la[:,2],'bo')
+    # p2, = ax1.plot(lN,least[2,0]*lN+least[2,1],'b-',label = '%.4f RK4' % least[2,0])
+
+    # ax1.plot(lN, la[:,3],'go')
+    # p3, = ax1.plot(lN,least[3,0]*lN+least[3,1],'g-',label = '%.4f Verlet' % least[3,0])
+
+    # ax1.plot(lN, la[:,4],'mo')
+    # p4, = ax1.plot(lN,least[4,0]*lN+least[4,1],'m-',label = '%.4f Adaptive RK4' % least[4,0])
+    # l0 = ax1.legend(handles=[p1,p2,p3,p4],loc=1)  
+
+
+    # ax1.set_ylabel(r'$r$ Log Error')
+    # ax1[1].set_ylabel(r'RK2')
+    # ax1[2].set_ylabel(r'RK4')
+    # ax1[3].set_ylabel(r'Verlet')
+    # ax1[4].set_ylabel(r'Adaptive RK4')
+    # ax1[4].set_xlabel(r'Log $N$')
